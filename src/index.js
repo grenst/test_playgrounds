7812a71846e285911168b80f3f191f4042a730b1
grenst <74575794+grenst@users.noreply.github.com>
2022-07-27 18:31:22 +0200

    added Graphic + fixes
    - Interactive graphic now shows how will change temperature in next 15h;
    - Added apearing after page is loaded (now fake data changes to actual in New York weather with transition);
    - Added gradient BG on page and content (with opacity)
    - Changed name to "FUN WEATHER APP"
    - Deleted logo...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let apiKey = "fe0e0a19e1606bdfe513d213f2be135a";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&appid=${apiKey}`;
+axios.get(url).then(getData);
 
 function getData(response) {
   let cityName = response.data.name;
@@ -20,6 +22,79 @@ function getData(response) {
   windCity.innerHTML = `Wind: ${windSp} km/h`;
   skyCity.innerHTML = `Sky: ${sky}`;
   document.getElementById("magic").style.opacity = "1";
+  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(graphic);
+}
+
+function graphic(res) {
+  let d1 = res.data.list[0].main.temp_max;
+  let d1r = Math.round(d1);
+  let g1 = persGraf(d1r);
+  let date1ms = res.data.list[0].dt * 1000;
+  let date1dt = new Date(date1ms);
+  let d2 = res.data.list[1].main.temp_max;
+  let d2r = Math.round(d2);
+  let g2 = persGraf(d2r);
+  let date2ms = res.data.list[1].dt * 1000;
+  let date2dt = new Date(date2ms);
+  let d3 = res.data.list[2].main.temp_max;
+  let d3r = Math.round(d3);
+  let g3 = persGraf(d3r);
+  let date3ms = res.data.list[2].dt * 1000;
+  let date3dt = new Date(date3ms);
+  let d4 = res.data.list[3].main.temp_max;
+  let d4r = Math.round(d4);
+  let g4 = persGraf(d4r);
+  let date4ms = res.data.list[3].dt * 1000;
+  let date4dt = new Date(date4ms);
+  let d5 = res.data.list[4].main.temp_max;
+  let d5r = Math.round(d5);
+  let g5 = persGraf(d5r);
+  let date5ms = res.data.list[4].dt * 1000;
+  let date5dt = new Date(date5ms);
+  const element = document.querySelector(".days-graph");
+  let polygon = `polygon(0% ${g1}%, 25% ${g2}%, 50% ${g3}%, 75% ${g4}%, 100% ${g5}%, 100% 100%, 0% 100%)`;
+  element.style.setProperty("--polygone", polygon);
+  let graphic = document.querySelector(".days-graph");
+  graphic.innerHTML = res.data.list[0].dt_txt;
+  const temp1 = document.querySelector(".point1");
+  const temp2 = document.querySelector(".point2");
+  const temp3 = document.querySelector(".point3");
+  const temp4 = document.querySelector(".point4");
+  const temp5 = document.querySelector(".point5");
+  temp1.innerHTML = `${d1r}°`;
+  temp2.innerHTML = `${d2r}°`;
+  temp3.innerHTML = `${d3r}°`;
+  temp4.innerHTML = `${d4r}°`;
+  temp5.innerHTML = `${d5r}°`;
+  temp1.style.setProperty("--x1", `${g1 + 15}px`);
+  temp2.style.setProperty("--x2", `${g2 + 15}px`);
+  temp3.style.setProperty("--x3", `${g3 + 15}px`);
+  temp4.style.setProperty("--x4", `${g4 + 15}px`);
+  temp5.style.setProperty("--x5", `${g5 + 15}px`);
+  let timmi1 = document.querySelector(".time1");
+  timmi1.innerHTML = `${zeroBefore(date1dt.getHours())}:${zeroBefore(date1dt.getMinutes())}`;
+  let timmi2 = document.querySelector(".time2");
+  timmi2.innerHTML = `${zeroBefore(date2dt.getHours())}:${zeroBefore(date2dt.getMinutes())}`;
+  let timmi3 = document.querySelector(".time3");
+  timmi3.innerHTML = `${zeroBefore(date3dt.getHours())}:${zeroBefore(date3dt.getMinutes())}`;
+  let timmi4 = document.querySelector(".time4");
+  timmi4.innerHTML = `${zeroBefore(date4dt.getHours())}:${zeroBefore(date4dt.getMinutes())}`;
+  let timmi5 = document.querySelector(".time5");
+  timmi5.innerHTML = `${zeroBefore(date5dt.getHours())}:${zeroBefore(date5dt.getMinutes())}`;
+}
+
+function zeroBefore(inver) {
+  if (inver < 10) {
+    return `0${inver}`;
+  } else {
+    return `${inver}`;
+  }
+}
+
+function persGraf(vari) {
+  let a = 100 - (vari - 10) * 5;
+  return a;
 }
 
 function success(pos) {
